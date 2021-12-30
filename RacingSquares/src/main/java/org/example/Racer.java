d1eae8468bd4bd80f9b8e8b0f32c231e689b5147
Ashwini5 <ashwiniainchwar5@gmail.com>
2021-12-31 00:00:02 +0530

    Replace locks by Atomic CAS

--- RacingSquares/src/main/java/org/example/Racer.java
+++ RacingSquares/src/main/java/org/example/Racer.java
@@ -1,14 +1,14 @@
 package org.example;
 
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Racer implements Runnable {
 
     private Integer position;
     private Random random;
     private Boolean isWinner;
-    private static Boolean isRaceWon = false;
-    private static Object sharedLock = new Object();
+    private static AtomicBoolean isRaceWon = new AtomicBoolean(false);
 
     public Racer() {
         this.position = 0;
@@ -28,10 +28,9 @@ public class Racer implements Runnable {
         if (this.position < Constants.END_X_DISTANCE) {
             this.position += random.nextInt(Constants.RACER_SPEED_MAX - Constants.RACER_SPEED_MIN) + Constants.RACER_SPEED_MIN;
             if (this.position >= Constants.END_X_DISTANCE) {
-                synchronized (sharedLock) {
-                    if (!isRaceWon) {
-                        isRaceWon = isWinner = true;
-                    }
+                boolean updateStatus = isRaceWon.compareAndSet(false, true);
+                if(updateStatus) {
+                    isWinner = true;
                 }
             }
             Thread.sleep(Constants.RACER_REFRESH_RATE_IN_MILLI_SECS);
