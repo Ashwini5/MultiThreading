599684d18aa134a14649fc78fe464f3d168754b4
Ashwini5 <ashwiniainchwar5@gmail.com>
2022-01-05 22:52:54 +0530

    Realtime rank calculations

--- RacingSquares/src/main/java/org/example/RaceController.java
+++ RacingSquares/src/main/java/org/example/RaceController.java
@@ -1,16 +1,32 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+
+import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.averagingDouble;
+import static java.util.stream.Collectors.groupingBy;
 
 public class RaceController {
 
+    public class RacerPositions extends ArrayList<ImmutablePair<Integer, Integer>> {}
+    private class RacerPositionsWithId extends ArrayList<ImmutablePair<Integer, ImmutablePair<Integer, Integer>>> {}
+
     private ArrayList<Racer> racers;
+    private final Lock pauseRacersLock = new ReentrantLock();
+    private final Condition condition = pauseRacersLock.newCondition();
+    private AtomicBoolean pauseRacers;
 
     public RaceController(int numberOfRacers) {
+        this.pauseRacers = new AtomicBoolean(false);
         this.initializeRacers(numberOfRacers);
     }
 
@@ -21,23 +37,67 @@ public class RaceController {
     public void initializeRacers(int numberOfRacers) {
         this.racers = new ArrayList<>();
         for (int i=0; i < numberOfRacers; i++) {
-            this.racers.add(new Racer());
+            this.racers.add(new Racer(this.pauseRacersLock, this.condition, this.pauseRacers));
         }
     }
 
-    public ArrayList<Integer> getRacerPositions() {
-        return (ArrayList<Integer>) this.racers.stream().map(Racer::getPosition).collect(Collectors.toList());
+    private RacerPositions computeRanks(RacerPositionsWithId racerPositionsWithId) {
+        RacerPositionsWithId unfinishedRacers = racerPositionsWithId.stream().filter(r -> r.right.left == 0).collect(Collectors.toCollection(RacerPositionsWithId::new));
+        RacerPositionsWithId finishedRacers = racerPositionsWithId.stream().filter(r -> r.right.left != 0).collect(Collectors.toCollection(RacerPositionsWithId::new));
+        Integer maxFinishedRacerRank = racerPositionsWithId.stream().mapToInt(r -> r.right.left).max().orElse(0);
+
+        unfinishedRacers.sort(Comparator.comparingInt(x -> x.right.right));
+        Collections.reverse(unfinishedRacers);
+        for (int i=0; i < unfinishedRacers.size(); i++) {
+            Integer racerId = unfinishedRacers.get(i).left;
+            Integer racerPosition = unfinishedRacers.get(i).right.right;
+            unfinishedRacers.set(i, new ImmutablePair<>(racerId, new ImmutablePair<>(maxFinishedRacerRank + i + 1, racerPosition)));
+        }
+
+        return Stream.of(finishedRacers, unfinishedRacers)
+            .flatMap(r -> r.stream())
+            .sorted(Comparator.comparingInt(r -> r.left))
+            .map(r -> r.right)
+            .collect(Collectors.toCollection(RacerPositions::new));
     }
 
-    public ArrayList<Boolean> getRacersWinningStatus() {
-        return (ArrayList<Boolean>) this.racers.stream().map(Racer::getIsWinner).collect(Collectors.toList());
+    public RacerPositions getRacerPositions() {
+        RacerPositionsWithId racerPositionsWithId;
+        this.pauseRace();
+        racerPositionsWithId =
+            this.racers
+                .stream()
+                .map(x -> new ImmutablePair<>(x.getId(), new ImmutablePair<>(x.getRank(), x.getPosition())))
+                .collect(Collectors.toCollection(RacerPositionsWithId::new));
+        this.unpauseRace();
+        return computeRanks(racerPositionsWithId);
     }
+
+//    public ArrayList<Boolean> getRacersWinningStatus() {
+//        return (ArrayList<Boolean>) this.racers.stream().map(Racer::getIsWinner).collect(Collectors.toList());
+//    }
 
    public void startRace() {
        ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(racers.size());
+
        int numberOfRacers = racers.size();
        for (int i=0; i < numberOfRacers; i++) {
            threadPoolExecutor.execute(racers.get(i));
        }
+   }
+
+   public void pauseRace() {
+       while(!this.pauseRacers.compareAndSet(false, true));
+   }
+
+   public void unpauseRace() {
+        this.pauseRacersLock.lock();
+        try {
+            if (this.pauseRacers.compareAndSet(true, false)) {
+                condition.signalAll();
+            }
+       } finally {
+            pauseRacersLock.unlock();
+        }
    }
 }
--- RacingSquares/src/main/java/org/example/Racer.java
+++ RacingSquares/src/main/java/org/example/Racer.java
@@ -2,22 +2,46 @@ package org.example;
 
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 
 public class Racer implements Runnable {
 
+    private Integer id;
+    private static Integer idCounter = 0;
     private Integer position;
     private Random random;
-    private Boolean isWinner;
-    private static AtomicBoolean isRaceWon = new AtomicBoolean(false);
+//    private Boolean isWinner;
+    private static AtomicInteger raceCompletedBy;
+//    private static AtomicBoolean isRaceWon = new AtomicBoolean(false);
+    private final Lock pauseRacerLock;
+    private final Condition condition;
+    private AtomicBoolean pauseRacer;
+    private Integer rank;
 
-    public Racer() {
+    public Racer(Lock pauseRacerLock, Condition condition, AtomicBoolean pauseRacer) {
+        this.id = idCounter++;
+        this.pauseRacerLock = pauseRacerLock;
+        this.condition = condition;
+        this.pauseRacer = pauseRacer;
         this.position = 0;
         this.random = new Random();
-        this.isWinner = false;
+//        this.isWinner = false;
+        raceCompletedBy = new AtomicInteger(0);
+        this.rank = 0;
     }
 
-    public Boolean getIsWinner(){
-        return this.isWinner;
+//    public Boolean getIsWinner(){
+//        return this.isWinner;
+//    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public Integer getRank() {
+        return this.rank;
     }
 
     public Integer getPosition() {
@@ -25,16 +49,28 @@ public class Racer implements Runnable {
     }
 
     public void move() throws InterruptedException {
+
+        if (this.pauseRacer.get()) {
+            pauseRacerLock.lock();
+            try {
+                condition.await();
+            } finally {
+                pauseRacerLock.unlock();
+            }
+        }
+
         if (this.position < Constants.END_X_DISTANCE) {
             this.position += random.nextInt(Constants.RACER_SPEED_MAX - Constants.RACER_SPEED_MIN) + Constants.RACER_SPEED_MIN;
             if (this.position >= Constants.END_X_DISTANCE) {
-                boolean updateStatus = isRaceWon.compareAndSet(false, true);
-                if(updateStatus) {
-                    isWinner = true;
+//                boolean updateStatus = isRaceWon.compareAndSet(false, true);
+//                if(updateStatus) {
+//                    isWinner = true;
+//                }
+                this.rank = raceCompletedBy.incrementAndGet();
+
                 }
             }
             Thread.sleep(Constants.RACER_REFRESH_RATE_IN_MILLI_SECS);
-        }
     }
 
     @Override
